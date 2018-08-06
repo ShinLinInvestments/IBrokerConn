c58a0a10b9116bec732e6f6e3e0b138af49ea155
Shinlin-macPro2008 <apple@appledeMBP.fios-router.home>
2018-08-05 20:03:57 -0400

    Restructured folder

--- util.tws.R
+++ util.tws.R
@@ -6,7 +6,11 @@ tws = twsConnect(clientId = 1, port = 7496)
 acct = reqAccountUpdates(tws)
 twsPortfolioValue(acct)
 
-seco = twsFuture('YM', exch='ECBOT', expiry='201806')
-pv0 = reqHistoricalData(tws, seco, barSize = '1 min', whatToShow = 'BID_ASK', duration = "1 D")
+seco = twsFuture('YM', exch='ECBOT', expiry='201809')
+seco = twsFuture('NQ', exch='GLOBEX', expiry='201809')
+pv0 = reqHistoricalData(tws, seco, barSize = '1 min', whatToShow = 'TRADES', useRTH = "0", duration = "1 M")
 pv1 = data.table(datetime = index(pv0), pv0)
 colnames(pv1) = c('datetime','Open','High','Low','Close','Vlm','wap','hasGaps','Count')
+
+twsDisconnect(tws)
+
