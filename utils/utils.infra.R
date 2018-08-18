effd73cd6a5ae9253789f4f0d3de1be69ea59364
ShinLin_MacPro2008 <24879504+ShinLinInvestments@users.noreply.github.com>
2018-08-18 15:50:21 -0400

    Extended functionality in getHistoricalData so user can define sdate and edate

--- utils/utils.infra.R
+++ utils/utils.infra.R
@@ -6,7 +6,7 @@ library(futile.logger)
 if(tolower(Sys.info()['sysname']) == 'darwin'){
     Sys.setlocale("LC_TIME", "en_US.UTF-8")
 } else if(tolower(Sys.info()['sysname']) == 'windows'){
-    Sys.setlocale("LC_TIME","English")
+    Sys.setlocale("LC_TIME", "English")
 }
 
 # Logging
@@ -14,6 +14,10 @@ flog.layout(layout.format('~t|~l|~n|~f|~m'))
 flog.threshold(DEBUG)
 
 # Infra functions
+x.p <- function(...) paste(..., sep='')
+
+x.isYmd <- function(d) grepl('^[1-2][0-9][0-9][0-9][0-1][0-9][0-3][0-9]$', as.character(d))
+
 x.checkCond <- function(cond, err.message){
     if(all(cond)) return()
     flog.fatal(err.message)
--- utils/utils.tws.R
+++ utils/utils.tws.R
@@ -1,16 +1,29 @@
 library(IBrokers)
 
-x.ib.getHistoricalData <- function(tws.conn, tws.contracts, ...){
+x.ib.getHistoricalData <- function(tws.conn, tws.contracts, start.date, end.date = format(Sys.Date(), '%Y%m%d'), ...){
+    x.checkCond(x.isYmd(start.date), paste('start.date =', start.date, 'not valid'))
+    x.checkCond(x.isYmd(end.date), paste('end.date =', end.date, 'not valid'))
     if(tolower(class(tws.contracts)) != 'twscontract'){
-        return(rbindlist(lapply(tws.contracts, function(tws.contract) x.ib.getHistoricalData(tws.conn, tws.contract, ...)), use.names = TRUE, fill = TRUE))
+        return(rbindlist(lapply(tws.contracts, function(tws.contract) x.ib.getHistoricalData(tws.conn, tws.contract, start.date, end.date, ...)), use.names = TRUE, fill = TRUE))
     }
     contract.specs.df = t(data.frame(value=unlist(tws.contracts)[which(!unlist(tws.contracts) %in% c('', '0'))]))
-    flog.info(paste('Retrieving', paste(colnames(contract.specs.df), '=', as.character(contract.specs.df), collapse = ', ')))
-    pv0 = reqHistoricalData(tws.conn, tws.contracts, ...)
-    pv1 = data.table(pv0)
-    all.datetimes = index(pv0)
-    pv1 = data.table(date = format(all.datetimes, '%Y%m%d'), intv = format(all.datetimes, '%H%M%S'), contract.specs.df[rep(1,nrow(pv1)),], pv1)
-    colnames(pv1) = tolower(gsub('([a-z])([A-Z])', '\\1.\\2', gsub('.*\\.', '', colnames(pv1))))
-    pv1
+    contract.specs.print = paste(colnames(contract.specs.df), '=', as.character(contract.specs.df), collapse = ', ')
+    cur.end.datetime = paste(end.date, '23:59:59')
+    cur.end.date = end.date
+    res = c()
+    while(cur.end.date >= start.date){
+        pv0 = reqHistoricalData(tws.conn, tws.contracts, endDateTime = cur.end.datetime, duration = "5 D", ...)
+        pv1 = data.table(pv0)
+        all.datetimes = index(pv0)
+        pv1 = data.table(date = format(all.datetimes, '%Y%m%d'), intv = format(all.datetimes, '%H:%M:%S'), contract.specs.df[rep(1,nrow(pv1)),], pv1)
+        colnames(pv1) = tolower(gsub('([a-z])([A-Z])', '\\1.\\2', gsub('.*\\.', '', colnames(pv1))))
+        pv1 = pv1[order(date, intv)]
+        res = rbind(pv1, res)
+        next.start.datetime = pv1[1, paste(date, intv)]
+        flog.info(paste('Retrieved', contract.specs.print, 'from', next.start.datetime, 'to', pv1[nrow(pv1), paste(date, intv)]))
+        cur.end.datetime = next.start.datetime
+        cur.end.date = pv1[1, date]
+    }
+    res
 }
 
