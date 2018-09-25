df63e51f9085a3cf6b973afd47b79436995ec09f
ShinLin_MacPro2008 <24879504+ShinLinInvestments@users.noreply.github.com>
2018-09-24 21:49:17 -0400

    Switched to CONTFUT

--- python.based/main.py
+++ python.based/main.py
@@ -4,15 +4,15 @@ import time
 app = IBApiMaster("127.0.0.1", 7496, 1)
 
 contractIB = ibapi.contract.Contract()
-contractIB.secType = "FUT"
-contractIB.lastTradeDateOrContractMonth = "201812"
+contractIB.secType = "CONTFUT"
 contractIB.symbol = "ES"
 contractIB.exchange = "GLOBEX"
 
-contractIB = app.resolveContractIB(contractIB, reqId = 43, maxWaitSecs = 20)
+contractIB = app.resolveContractIB(contractIB, reqId = 43, maxWaitSecs = 40)
 print(contractIB)
 
 currentYMDHMS = time.strftime("%Y%m%d %H:%M:%S", time.localtime())
+currentYMDHMS = "20180915 10:00:00"
 historic_data = app.getHistoricalData(reqId = 51, contractIB = contractIB, maxWaitSecs = 200, endDateTime = currentYMDHMS,
                                       durationStr = '1 W', barSizeSetting = '30 secs', whatToShow = 'TRADES', useRTH = 0,
                                       formatDate = 1, keepUpToDate = False)
