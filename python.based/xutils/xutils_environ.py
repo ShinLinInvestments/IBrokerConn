b7c26143c7f192bbae2b394a9d0a5693dde36b06
ShinLinInvestments <24879504+ShinLinInvestments@users.noreply.github.com>
2018-10-15 09:09:52 +0800

    Update xutils_environ.py

--- python.based/xutils/xutils_environ.py
+++ python.based/xutils/xutils_environ.py
@@ -7,4 +7,4 @@ os.environ['TZ'] = config_manager['xutils']['time_zone']
 logging.basicConfig(format = '%(asctime)s|%(levelname)s|%(funcName)s|%(thread)d|%(filename)s:%(lineno)d|%(message)s',
                     datefmt='%Y%m%d.%H%M%S.%Z')
 xlog = logging.getLogger('SLFD')
-xlog.setLevel(level = config_manager['xutils']['log_level'],)
+xlog.setLevel(level = config_manager['xutils']['log_level'])
