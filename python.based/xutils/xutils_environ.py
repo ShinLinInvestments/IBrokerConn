c429af35976cbf0e88a294b2b920928c2b056d2f
ShinLinInvestments <24879504+ShinLinInvestments@users.noreply.github.com>
2018-10-15 09:10:48 +0800

    Minor fix

--- python.based/xutils/xutils_environ.py
+++ python.based/xutils/xutils_environ.py
@@ -8,3 +8,4 @@ logging.basicConfig(format = '%(asctime)s|%(levelname)s|%(funcName)s|%(thread)d|
                     datefmt='%Y%m%d.%H%M%S.%Z')
 xlog = logging.getLogger('SLFD')
 xlog.setLevel(level = config_manager['xutils']['log_level'])
+
