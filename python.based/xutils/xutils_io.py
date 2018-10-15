3896a435986388a1dd61c0561d5ff799345f6907
ShinLinInvestments <24879504+ShinLinInvestments@users.noreply.github.com>
2018-10-15 20:15:10 +0800

    Finished initial draft of read_csv

--- python.based/xutils/xutils_io.py
+++ python.based/xutils/xutils_io.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import os
 
-def read_csv(path_prefix, sdatetime, edatetime):
-    filenames = os.listdir(path_prefix)
+def read_csv(path_prefix:str, datetimes:list):
+    filenames = [path_prefix + x + '.csv' for x in datetimes]
+    res = pd.DataFrame()
+    for x in map(pd.read_csv, filenames):
+        res = pd.concat([res, x])
+    res.index = list(range(len(res)))
+    return res
 
 def write_csv(data_table:pd.DataFrame, path_prefix, col_datetime):
+    if len(data_table) == 0: return
     assert col_datetime in data_table.columns.values
     all_dts = data_table[col_datetime]
     
