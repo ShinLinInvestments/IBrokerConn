e8d3f05fe127dcf1c6548efcf0220cc679733e8b
ShinLinInvestments <24879504+ShinLinInvestments@users.noreply.github.com>
2018-10-21 17:43:17 +0800

    from eim

--- python.based/xutils/xutils_io.py
+++ python.based/xutils/xutils_io.py
@@ -15,18 +15,38 @@ def write_csv(data_table:pd.DataFrame, path_prefix, col_datetime):
     print(type(all_dts))
 
 ##
+import pandas as pd
+import numpy as np
+import glob
+import multiprocessing as mc
+import os
+from xutils.xutils_environ import xlog, xutils_num_cores
+
 def check_path_prefix(path_prefix:str):
     path_last_dir = os.path.dirname(path_prefix)
     if not os.path.exists(path_last_dir):
         os.mkdir(path_last_dir)
     return
 
-def read_csv(path_prefix:str, datetimes:list):
-    filenames = [path_prefix + x + '.csv' for x in datetimes]
-    res = pd.DataFrame()
-    for x in map(pd.read_csv, filenames):
-        res = pd.concat([res, x])
-    res.index = list(range(len(res)))
+def read_csv(path_prefix:str, datetimes:list, allow_missing:bool = True):
+    assert len(datetimes) > 0, "input datetimes is empty!"
+    assert type(datetimes[0]) == type(""), "input datetimes[0] = " + str(datetimes[0]) + " is not str"
+    filenames = []
+    for i, x in enumerate(datetimes):
+        assert type(x) == type(""), "input datetimes[" + str(i) + "] = " + str(x) + " is not str"
+        assert not ' ' in x, "input datetimes[" + str(i) + "] = " + str(x) + " should not have space in it"
+        filenames.append(path_prefix + x + '.csv')
+    res = []
+    for f in filenames:
+        try:
+            curres = pd.read_csv(f)
+            res.append(curres)
+            xlog.info("Read " + str(len(curres)) + " lines from " + f)
+        except FileNotFoundError:
+            if not allow_missing:
+                xlog.critical(f + " not found")
+                raise
+    res = pd.concat(res)
     return res
 
 def _write_single_csv(data_table:pd.DataFrame, rows_to_print:int, path_output:str, index = False):
@@ -45,4 +65,5 @@ def write_csv(df:pd.DataFrame, path_prefix, col_datetime, file_freq:int = 8, ind
     xlog.info("Planning to write " + str(len(all_dts)) + " files for: " + ','.join(all_dts))
     for x in all_dts:
         _write_single_csv(df, (all_dtss == x), path_prefix + x + '.csv', index=index)
+
 
